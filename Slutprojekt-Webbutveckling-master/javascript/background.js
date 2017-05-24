bddd0f5dedf2cc561a099ac39084df0bbe97a872
p0ppyy <maksimilian.andersen@gmail.com>
2017-05-24 03:03:43 +0200

    Add files via upload

--- Slutprojekt-Webbutveckling-master/javascript/background.js
+++ Slutprojekt-Webbutveckling-master/javascript/background.js
@@ -1,10 +1,18 @@
 var date = new Date();
 var time = date.getSeconds();
-$(document).ready(function(e) {
-	console.log(time);
-	if(time % 2 > 0){
-		$("body").css("background-image", "url('bg1.png')" );	
-	}else{
-		$("body").css("background-image", "url('bg2.png')");
-	}
+$(document).ready(function() {
+	changeBackground();
 });
+
+function changeBackground(){
+	if($(this).width() > 520){
+		if(time % 2 > 0){
+			$("body").css("background-image", "url('bg1.png')" );
+		}else{
+			$("body").css("background-image", "url('bg2.png')");
+		}
+	}else {
+		$("body").css("background-image", "none");
+	}
+
+}
